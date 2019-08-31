7963c592564b8dcd462f961e4ff304606e222243
dbx834 <orsha.pk@gmail.com>
2019-08-31 11:18:16 +0530

    patch

--- src/pages/events.jsx
+++ src/pages/events.jsx
@@ -113,12 +113,14 @@ const eventsData = [
     title: 'Connection as Life Energy – NVC Level 1 (Mumbai)',
     allDay: true,
     start: new Date(2019, 9, 12),
-    end: new Date(2019, 9, 13),
+    end: new Date(2019, 9, 14),
     resource: {
       abstract:
-        'A path of Nonviolence offers both a deeply-rooted consciousness of oneness, as well as very practical tools to live this more and more in our daily lives. In re-evaluating some of our habitual and conditioned thinking, we see how much of it is life-alienating and based on a culture of scarcity and separation. We’ll learn the skills to be able express ourselves with more clarity and to hear others’ true (and harmless) message, which in turn leads to more connection between us. When we’re connected as human beings, we’re in our natural state of wanting to give and collaborate with others.',
+        'A path of Nonviolence offers both a deeply-rooted consciousness of oneness, as well as very practical tools to live this more and more in our daily lives.',
       cover:
         'https://www.joylivinglearning.org/content-assets/2018-events/nvc-mumbai.jpeg',
+      link:
+        'https://www.joylivinglearning.org/events/featured/2019/oct/connection-as-life-energy',
     },
   },
 ]
